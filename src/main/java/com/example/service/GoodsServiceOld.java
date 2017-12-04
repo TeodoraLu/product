baf1f2979d215fff75be72d20a4d3beacfe69e63
zhanghao <hao.zhang@nanguadl.com>
2017-12-04 15:17:09 +0800

    add

---
+++ src/main/java/com/example/service/GoodsServiceOld.java
@@ -0,0 +1,38 @@
+package com.example.service;
+
+import com.example.bean.GoodsStock;
+import com.example.dao.mapper.GoodsStockMapper;
+import com.example.dao.mapper.GoodsStockQueryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by zhangh on 2017/12/4.
+ */
+
+@Service
+public class GoodsServiceOld {
+
+    @Autowired
+    private GoodsStockQueryMapper goodsStockQueryMapper;
+
+    @Autowired
+    private GoodsStockMapper goodsStockMapper;
+
+    public List<GoodsStock> query(){
+        return goodsStockQueryMapper.query();
+    }
+    public int save(GoodsStock goodsStock) {
+        return goodsStockMapper.insertSelective(goodsStock);
+    }
+
+    public GoodsStock queryById(Integer id){
+        return goodsStockMapper.selectByPrimaryKey(id);
+    }
+
+    public void update(GoodsStock goodsStock){
+        goodsStockMapper.updateByPrimaryKeySelective(goodsStock);
+    }
+}
